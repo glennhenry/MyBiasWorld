fb3168249bdaf8f0d62986d8680971b9b9dafa3a
glennhenry <glennhenrybdg05@gmail.com>
2026-08-28 19:31:51 +0700

    Update writetopic.html to use textfield fragment

--- src/main/kotlin/portal/domain/cafe/CafeRoutes.kt
+++ src/main/kotlin/portal/domain/cafe/CafeRoutes.kt
@@ -161,10 +161,10 @@ class CafeRoutes(private val serverContext: ServerContext) : RouteHandler {
 
                 val post = JSON.decode<WriteTopicPayload>(call.receiveText())
 
-                if (post.title.length < 8 || post.content.length < 20) {
+                if (post.title.length < 10 || post.content.length < 20) {
                     call.respond(
                         HttpStatusCode.BadRequest,
-                        "Title should be at least 8 characters and content should contains at least 20 characters"
+                        "Title should be at least 10 characters and content should contains at least 20 characters"
                     )
                     return@handle
                 }

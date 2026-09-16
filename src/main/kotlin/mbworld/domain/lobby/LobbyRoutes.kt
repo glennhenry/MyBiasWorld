33becee886e281cbbeff14bd67a2ca4522691b74
glennhenry <glennhenrybdg05@gmail.com>
2026-09-16 18:11:13 +0700

    Implement basic events fetch system on lobby

--- src/main/kotlin/mbworld/domain/lobby/LobbyRoutes.kt
+++ src/main/kotlin/mbworld/domain/lobby/LobbyRoutes.kt
@@ -1,17 +1,22 @@
 package mbworld.domain.lobby
 
+import encore.fancam.Fancam
 import encore.route.RouteHandler
 import encore.route.guard
+import encore.route.guard.NoAuthGuard
+import encore.serialization.JSON
 import encore.time.TimeCenter
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
+import kotlinx.serialization.Serializable
 import mbworld.context.ServerContext
 import mbworld.domain.Members
 import mbworld.domain.lobby.model.LobbyModel
 import mbworld.routes.guard.OptionalAccountGuard
 import mbworld.routes.guard.getAccountData
 import java.text.SimpleDateFormat
+import kotlin.random.Random
 
 /**
  * Routes for lobby, which is the root page `/`.
@@ -34,5 +39,31 @@ class LobbyRoutes(serverContext: ServerContext) : RouteHandler {
                 call.respond(ThymeleafContent("lobby", mapOf("data" to data)))
             }
         }
+
+        get("/events") {
+            guard(call, NoAuthGuard) {
+                val response = EventsResponse(
+                    listOf(
+                        Event("First event received (${Random.nextInt(1, 200)})"),
+                        Event("Hello this is an event (${Random.nextInt(1, 200)})")
+                    )
+                )
+
+                Fancam.debug { "Request to /events" }
+
+                call.respond(JSON.encode(response))
+            }
+        }
     }
 }
+
+
+@Serializable
+data class EventsResponse(
+    val events: List<Event>
+)
+
+@Serializable
+data class Event(
+    val text: String
+)
